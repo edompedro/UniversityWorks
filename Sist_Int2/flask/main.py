5823db7dcf6e02bb76a8441ffbdb2b10a1c5ec50
edompedro <pedroedomnunes@gmail.com>
2024-08-19 10:22:32 -0300

    :sparkles: feat: Add 2024 jobs

---
+++ Sist_Int2/flask/main.py
@@ -0,0 +1,61 @@
+from flask import Flask, json, render_template, redirect, session, request
+from flask import redirect
+
+app = Flask(__name__)
+
+app.secret_key = "adim"
+
+# abrir o json e lê os dados
+with open("alunos.json", "r") as f:
+    alunos = json.load(f)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route('/user/<username>')
+def user(username):
+    return f'Hello, {username}!'
+
+@app.route("/about")
+def about():
+    text = "This is the about page."
+    return render_template("about.html", text=text)
+
+# ROUTE TO ADD
+@app.route("/chamada", methods=["GET", "POST", "DELETE"])
+def chamada():      
+    if request.method == "POST" and request.form['type'] == 'Adicionar':
+        novo_nome = request.form["nome"]
+        nova_matricula = request.form["matricula"]
+        print(novo_nome, nova_matricula)
+        alunos[str(novo_nome)] = int(nova_matricula)
+        # Salvar num arquivo json
+        with open("alunos.json", "w") as f:
+            json.dump(alunos, f)
+
+    return render_template("chamada.html", alunos=alunos)
+    
+# ROUTE TO DELETE
+@app.route('/chamada/<nome>/<type>', methods=["POST"])
+def removeUser(nome,type):
+    if request.method == "POST" and type == 'DELETE':
+        del alunos[nome]
+        with open("alunos.json", "w") as f:
+            json.dump(alunos, f)
+    return render_template("chamada.html", alunos=alunos)
+
+# ROUTE TO SIGNIN
+@app.route("/signin", methods=["POST"])
+def signin():
+    if request.method == "POST":
+        email = request.form["exampleInputEmail1"]
+        password = request.form["exampleInputPassword1"]
+        app.secret_key = email
+        session["username"] = email
+        print(session.items())  # dict_items([('username', value)])
+        return redirect(f"/user/{app.secret_key}")
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
